98888cf18310776f304e945b361199259e6b0422
NikKonshin <konbshin1996@gmail.c0m>
2022-04-30 12:54:06 +0300

    Добавил функцию printMessage

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,4 +5,9 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
     }
+
+   private static void printMessage(){
+       String a = "aaaa";
+       System.out.println(a);
+   }
 }
