6c9d02e7340dd217a60a72f1f5b010e58ca3d992
NikKonshin <konbshin1996@gmail.c0m>
2022-05-22 16:41:50 +0300

    HomeWork3

--- src/com/company/Lesson3.java
+++ src/com/company/Lesson3.java
@@ -9,36 +9,42 @@ public class Lesson3 {
     public static void main(String[] args) {
         sc = new Scanner(System.in);
         random = new Random();
-        int[] array = {0,1,2,3,4,5,6,7,8,9,10};
-        for (int i = 0; i < array.length; i++){ }
+        int[] array = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        for (int i = 0; i < array.length; i++) { }
         System.out.println("Hello player, What's your name?");
         String name = sc.nextLine();
         System.out.printf("%s do you want play?\n", name);
         System.out.println("yes/no\n");
         String start = sc.nextLine();
-        switch (start) {
-            case "yes":
-                System.out.println("guess the number from 0 to 10");
-
-                int q = generator(array);
-                System.out.println(q);
-                int userAnswer = sc.nextInt();
-
-                for (int i = 2; i >= 1; i--) {
-                    System.out.println("You have " + i + " attempts,");
+        do {
+            switch (start) {
+                case "yes":
+                    System.out.println("\nguess the number from 0 to " + (array.length - 1));
+                    int q = generator(array);
+                    System.out.println(q);
+                    int userAnswer = sc.nextInt();
+                    for (int j = 2; j >= 1; j--) {
+                        if (q != userAnswer) {
+                            System.out.println("sorry, not correct.");
+                            System.out.println("You have " + j + " attempts.");
+                            userAnswer = sc.nextInt();
+                        }
+                    }
                     if (q != userAnswer) {
-                        System.out.println("oh no");
-                        userAnswer = sc.nextInt();
+                        System.out.printf("you lose.");
+                        System.out.print("Repeat the game?\n yes / no \n");
                     }
-                    System.out.printf("you loh");
-                    if (q == userAnswer){
-                        System.out.println("Congratulations you win");
+                    if (q == userAnswer) {
+                        System.out.println("*** Congratulations you win ***");
+                        System.out.print("end game?\n yes / no \n");
                     }
-                }break;
-            case "no":
-                System.out.printf("too bad");
-                break;
-        }
+                    break;
+                case "no":
+                    System.out.printf("too bad.\n");
+                    System.out.println("exit?");
+                    break;
+            }
+        }while (sc.next().equals("no"));
     }
     static int generator(int[] array){
         int randomM = random.nextInt(array.length);
